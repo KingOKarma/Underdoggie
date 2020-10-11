a325eab8778184b56fdc896ab24302def5736b6c
KingOKarma <kainesimpkins782@gmail.com>
2020-10-11 03:14:57 +0100

    embed and say

--- src/commands/fun/say.js
+++ src/commands/fun/say.js
@@ -5,7 +5,6 @@ module.exports = {
     run: (_, message, args) => {
 
         message.delete({timeout: 25});
-        if (args[0] === undefined) return 
         message.channel.send(args.join(' '))
 
     }
